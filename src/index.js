59bc3d9d8e4a114be3803957fa738acf6da705e5
Paul Palkin <111692145+Bestpaul73@users.noreply.github.com>
2024-10-14 00:04:24 +0300

    hw10

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 'use strict';
 
+const comments = [
+  {
+    name: 'Глеб Фокин',
+    date: '12.02.22 12:18',
+    text: 'Это будет первый комментарий на этой странице',
+    likes: 3,
+    isLiked: false,
+    isEdit: false,
+  },
+  {
+    name: 'Варвара Н.',
+    date: '13.02.22 19:22',
+    text: 'Мне нравится как оформлена эта страница! ❤',
+    likes: 75,
+    isLiked: true,
+    isEdit: false,
+  },
+];
+
 const getMsgDateTime = () => {
   const msgDateTime = new Date();
-  return `${msgDateTime.getDate()}.${msgDateTime.getMonth().toString().padStart(2, '0')}.${msgDateTime
+  return `${msgDateTime.getDate()}.${(msgDateTime.getMonth() + 1).toString().padStart(2, '0')}.${msgDateTime
     .getFullYear()
     .toString()
     .slice(2)} 
@@ -13,11 +32,129 @@ const commentsEl = document.querySelector('.comments');
 const inputEl = document.querySelector('.add-form-name');
 const textEl = document.querySelector('.add-form-text');
 const addButtonEl = document.querySelector('.add-form-button');
-const delButtonEl = document.querySelector('.del-form-button');
 
 addButtonEl.disabled = true;
 let userName = '';
 let userText = '';
+
+const handleLike = (e) => {
+  //два одинаковых способа достать индекс
+  const index = e.target.getAttribute('data-index');
+  console.log(index);
+  // const index = likeBtnElelement.dataset.index;
+  comments[index].isLiked ? comments[index].likes-- : comments[index].likes++;
+  comments[index].isLiked = !comments[index].isLiked;
+  renderComments();
+};
+
+const handleDelete = (e) => {
+  // console.dir(e.target);
+  const index = e.target.getAttribute('data-index');
+  comments.splice(index, 1);
+  console.log(index);
+  renderComments();
+};
+
+const handleEdit = (e) => {
+  const index = e.target.getAttribute('data-index');
+  comments[index].isEdit = true;
+  renderComments();
+
+  const currentComment = document.querySelectorAll('.comment')[index];
+  currentComment.querySelector('.edit-form-text').focus();
+};
+
+const handleSave = (e) => {
+  const index = e.target.getAttribute('data-index');
+  const currentComment = document.querySelectorAll('.comment')[index];
+  comments[index].text = currentComment.querySelector('.edit-form-text').value;
+  comments[index].isEdit = false;
+
+  renderComments();
+};
+
+const initEventListeners = () => {
+  const likeBtnElements = document.querySelectorAll('.like-button');
+  for (const likeBtnElement of likeBtnElements) {
+    likeBtnElement.addEventListener('click', (e) => handleLike(e));
+  }
+
+  const delBtnElements = document.querySelectorAll('.bin-button');
+  for (const delBtnElement of delBtnElements) {
+    delBtnElement.addEventListener('click', (e) => handleDelete(e));
+  }
+
+  const editBtnElements = document.querySelectorAll('.edit-button');
+  for (const editBtnElement of editBtnElements) {
+    editBtnElement.addEventListener('click', (e) => handleEdit(e));
+  }
+
+  const saveBtnElements = document.querySelectorAll('.save-button');
+  for (const saveBtnElement of saveBtnElements) {
+    saveBtnElement.addEventListener('click', (e) => handleSave(e));
+  }
+};
+
+const renderComments = () => {
+  commentsEl.innerHTML = comments
+    .map((comment, index) => {
+      return `<li class="comment">
+          <div class="comment-header">
+            <div>${comment.name}</div>
+            <div>${comment.date}</div>
+          </div>
+          <div class="comment-body">
+            ${
+              comment.isEdit
+                ? `<textarea type="textarea" class="edit-form-text"
+                    rows="2">${comment.text}</textarea>`
+                : `<div class="comment-text">${comment.text}</div>`
+            }
+            
+          </div>
+          <div class="comment-footer">
+            <div class="likes">
+              <span class="likes-counter">${comment.likes}</span>
+              <button 
+                data-index="${index}" 
+                class="like-button${comment.isLiked ? ' -active-like' : ''}">
+              </button>
+            </div>
+
+            <div class="controls">
+              ${
+                comment.isEdit
+                  ? `<button 
+                      data-index="${index}" 
+                      class="save-button"><img src="./img/disk.png" alt="Save" />
+                    </button>`
+                  : `<button 
+                      data-index="${index}" 
+                        class="edit-button"><img src="./img/edit.png" alt="Edit" />
+                    </button>`
+              }
+
+              <button 
+                data-index="${index}"
+                class="bin-button"><img src="./img/free-icon-delete-1345925.png" alt="Bin" />
+              </button>
+            </div>
+          </div>
+        </li>`;
+    })
+    .join('');
+
+  inputEl.value = '';
+  textEl.value = '';
+  userName = '';
+  userText = '';
+  addButtonEl.disabled = true;
+  // delButtonEl.disabled = false;
+
+  initEventListeners();
+};
+
+renderComments();
 
 const validateField = (element) => {
   let content = element.value.trim();
@@ -40,26 +177,16 @@ textEl.addEventListener('input', () => {
 });
 
 const addComment = () => {
-  commentsEl.innerHTML += `<li class="comment">
-          <div class="comment-header">
-            <div>${userName}</div>
-            <div>${getMsgDateTime()}</div>
-          </div>
-          <div class="comment-body">
-            <div class="comment-text">${userText}</div>
-          </div>
-          <div class="comment-footer">
-            <div class="likes">
-              <span class="likes-counter">0</span>
-              <button class="like-button"></button>
-            </div>
-            <button class="bin-button"><img src="./img/free-icon-delete-1345925.png" alt="Bin" /></button>
-          </div>
-        </li>`;
-  inputEl.value = '';
-  textEl.value = '';
-  addButtonEl.disabled = true;
-  delButtonEl.disabled = false;
+  const newComment = {
+    name: inputEl.value,
+    text: textEl.value,
+    date: getMsgDateTime(),
+    likes: 0,
+    isLiked: false,
+    isEdit: false,
+  };
+  comments.push(newComment);
+  renderComments();
 };
 
 addButtonEl.addEventListener('click', () => {
@@ -68,9 +195,4 @@ addButtonEl.addEventListener('click', () => {
 
 document.addEventListener('keyup', (e) => {
   if (e.key === 'Enter' && !addButtonEl.disabled) addComment();
-});
-
-delButtonEl.addEventListener('click', () => {
-  commentsEl.innerHTML = commentsEl.innerHTML.slice(0, commentsEl.innerHTML.lastIndexOf('<li class="comment"'));
-  if (!commentsEl.innerHTML.trim()) delButtonEl.disabled = true;
 });
